e83341ec7c6f8f518bbe3d576a5752427b72563f
Andrii Lata <82955441+AndriiLata@users.noreply.github.com>
2025-05-31 08:32:34 +0200

    Update PingPerfectClientImpl.java

--- backend/src/main/java/com/example/providercomparison/dto/provider/pingperfect/PingPerfectClientImpl.java
+++ backend/src/main/java/com/example/providercomparison/dto/provider/pingperfect/PingPerfectClientImpl.java
@@ -1,4 +1,3 @@
-// src/main/java/com/example/providercomparison/dto/provider/pingperfect/PingPerfectClientImpl.java
 package com.example.providercomparison.dto.provider.pingperfect;
 
 import com.example.providercomparison.config.PingPerfectProperties;
